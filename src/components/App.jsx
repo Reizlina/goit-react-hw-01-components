f32a43ae566902b92f5d37328b51a33594e76385
Reizlina Tanya <reizlina.tanya@gmail.com>
2022-06-21 21:10:51 +0300

    3/4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import user from 'user.json';
+import data from 'data.json';
+import friends from 'friends.json';
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
+
+//! Profile
 export const App = () => {
+  const { username, tag, location, avatar, stats } = user;
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />;
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,17 @@
+import css from './Statistics.module.css';
+
+export const Statistics = ({ stats, title }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.statList}>
+        {stats.map(({ id, label, percentage }) => (
+          <li className={css.item} key={id}>
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
